31bcdbcb12e257c671e3cdf02fef3527cdf78469
hanzha-dmytro <54111469+hanzha-dmytro@users.noreply.github.com>
2022-02-16 20:17:00 +0000

    Form components

---
+++ resources/js/vue/components/backend/form/FormInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label class="form-label" v-if="label">{{ label }}</label>
+        <input :value="value" :type="type" :placeholder="placeholder" :class="['form-control', {'is-invalid': errors.length}]" :disabled="disabled" @input="$emit('input', $event.target.value)">
+        <span class="invalid-feedback fw-bold" role="alert">{{ errors ? errors[0] : '' }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                type: [String, Number],
+                default: null,
+            },
+            type: {
+                type: String,
+                required: true,
+                validator: (value) => ['email', 'number', 'password', 'text'].includes(value),
+            },
+            label: {
+                type: String,
+                default: null,
+            },
+            placeholder: {
+                type: String,
+                default: null,
+            },
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+            errors: {
+                type: Array,
+                default: () => [],
+            },
+        },
+    }
+</script>
---
+++ resources/js/vue/components/backend/form/FormTextArea.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <label class="form-label" v-if="label">{{ label }}</label>
+        <textarea :value="value" :rows="rows" :placeholder="placeholder" :class="['form-control', {'is-invalid': errors.length}]" :disabled="disabled" @input="$emit('input', $event.target.value)"></textarea>
+        <span class="invalid-feedback fw-bold" role="alert">{{ errors ? errors[0] : '' }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                type: String,
+                default: null,
+            },
+            rows: {
+                type: Number,
+                default: 5,
+            },
+            label: {
+                type: String,
+                default: null,
+            },
+            placeholder: {
+                type: String,
+                default: null,
+            },
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+            errors: {
+                type: Array,
+                default: () => [],
+            },
+        },
+    }
+</script>
