2f3a54427938099d18d0c398031c6f0e2ab57ec9
hanzha-dmytro <54111469+hanzha-dmytro@users.noreply.github.com>
2023-03-18 15:39:32 +0200

    HTML Editor & DatePicker components

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,11 @@ module.exports = {
                         context: './resources'
                     }
                 }
-            }
+            },
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+                use: [ 'raw-loader' ]
+            },
         ]
     },
     output: {
